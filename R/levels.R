cb3566a7f15c694244df3350365e2e36e101437a
abichat <a.bichat@yahoo.fr>
2019-04-02 10:33:51 +0200

    last_level()

--- R/levels.R
+++ R/levels.R
@@ -9,15 +9,39 @@
 #' @export
 #'
 #' @examples
-#' lineage <- "k__Bacteria|p__Verrucomicrobia|c__Verrucomicrobiae"
-#' is_level(lineage, "class")
-#' is_level(lineage, "genus")
-is_level <- function(lineage, level = c("kingdom", "phylum", "class", "order",
+#' lineage1 <- "k__Bacteria|p__Verrucomicrobia|c__Verrucomicrobiae"
+#' lineage2 <- "k__Bacteria|p__Firmicutes|c__Clostridia"
+#' is_level(c(lineage1, lineage2), "class")
+#' is_level(c(lineage1, lineage2), "order")
+is_level <- function(lineage,
+                     level = c("kingdom", "phylum", "class", "order",
                                "family", "genus", "species", "strain")) {
+    level <- match.arg(level)
+    letter <- ifelse(level == "strain", "t", str_sub(level, end = 1))
 
-  level <- match.arg(level)
-  letter <- ifelse(level == "strain", "t", str_sub(level, end = 1))
+    str_detect(lineage, paste0(letter, "__[a-zA-Z0-9_]*$"))
+  }
 
-  str_detect(lineage, paste0(letter, "__[a-zA-Z0-9_]*$"))
+
+#' Extract the last level of a lineage
+#'
+#' @param lineage string.
+#' @param same logical. Do the lineage must be at the same depth? Default to TRUE.
+#'
+#' @return A string.
+#' @importFrom stringr str_count str_remove
+#' @export
+#'
+#' @examples
+#' lineage1 <- "k__Bacteria|p__Verrucomicrobia|c__Verrucomicrobiae"
+#' lineage2 <- "k__Bacteria|p__Firmicutes|c__Clostridia"
+#' last_level(c(lineage1, lineage2))
+last_level <- function(lineage, same = TRUE) {
+  if (same) {
+    if (!all(str_count(lineage, "\\|") == str_count(lineage[1], "\\|"))) {
+      stop("Lineages don't have the same depth")
+    }
+  }
+
+  str_remove(lineage, ".*__")
 }
-
